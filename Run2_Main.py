4224b0cad1f8f04dc0b6ce6a1484543dfd06971b
abhivoora9 <122940547+abhivoora9@users.noreply.github.com>
2023-01-19 20:44:41 -0600

    Update Run2_Main.py

--- Run2_Main.py
+++ Run2_Main.py
@@ -13,11 +13,11 @@ movement_motors.move(45,'cm') # moving the robot forwards
 right_motor.run_for_degrees(600)# turning to mission
 #extension_motor.set_default_speed(100)
 
-extension_motor.run_for_degrees(-40,100)#taking energy cubes
-extension_motor.run_for_degrees(40,100)
-extension_motor.run_for_degrees(-40,100)
-extension_motor.run_for_degrees(40,100)
-extension_motor.run_for_degrees(-40,100)
+extension_motor.run_for_degrees(-40,80)#taking energy cubes
+extension_motor.run_for_degrees(40,80)
+extension_motor.run_for_degrees(-40,80)
+extension_motor.run_for_degrees(40,80)
+extension_motor.run_for_degrees(-40,80)
 #start of solar mission
 movement_motors.move(-8,'cm')# moves back
 #extension_motor.run_for_degrees(-50)#Turning to solar mission
@@ -25,13 +25,13 @@ right_motor.run_for_degrees(-590)
 #left_motor.run_for_degrees(80,'cm')
 movement_motors.move(25,'cm')
 #left_motor.run_for_degrees(-100)
-extension_motor.run_for_degrees(40,100)
-movement_motors.move(18,'cm')
-movement_motors.move(-15,'cm')
+extension_motor.run_for_degrees(40,100)#the wheels for the robot moves 40 degrees at 100 speed
+movement_motors.move(18,'cm')#robot moves 18 centimieters forward 
+movement_motors.move(-15,'cm')#robot moves 15 centimieters backwards
 left_motor.run_for_degrees(-250)
 movement_motors.move(23,'cm')
-movement_motors.move(-23,'cm')
-left_motor.run_for_degrees(-235)
-extension_motor.run_for_degrees(-40,100)
-movement_motors.move(23,'cm')
-right_motor.run_for_degrees(635)
+movement_motors.move(-34,'cm')
+right_motor.run_for_degrees(-850)
+movement_motors.move(10,'cm')
+right_motor.run_for_degrees(-400)
+movement_motors.move(52,'cm')
